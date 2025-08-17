b585415fcd9a88e8e8b0fafb2adc83d98666b2f6
KeorapetseCode <kmpoloke@student.wethinkcode.co.za>
2025-08-17 17:48:06 +0200

    landing page and folder restructure

--- app/screens/landing-page/landing-page.ts
+++ app/screens/landing-page/landing-page.ts
@@ -1,5 +1,5 @@
 import { EventData, Page } from '@nativescript/core'
-import { HelloWorldModel } from './main-view-model'
+import { HelloWorldModel } from './landing-page-model'
 
 export function navigatingTo(args: EventData) {
   const page = <Page>args.object
