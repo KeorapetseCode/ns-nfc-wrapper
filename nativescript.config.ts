c4f9dd8df3227546796c70eaf20ee2b294a5ad14
KeorapetseCode <kmpoloke@student.wethinkcode.co.za>
2025-12-08 18:34:03 +0200

    front end cleanup: remove yahoo login button and related styles, update project identifier

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.nsnfcwrapper',
+  id: 'com.keorapetsecode.nsnfcwrapper',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {
